5eaad9679fed60a71337e3dedeff2dda88a0599e
javad <javadalizadeh1993@gmail.com>
2022-07-20 14:29:29 +0430

    AddIdentity

--- App.Domain.Core/AppUser.cs
+++ App.Domain.Core/AppUser.cs
@@ -1,12 +1,12 @@
-﻿//using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 
-//namespace App.Domain.Core.Entities;
+namespace App.Domain.Core.Entities;
 
-//public class AppUser : IdentityUser<int>
-//{
-//    public string FirstName { get; set; }
-//    public string LastName { get; set; }
-//    public bool IsActive { get; set; }
-//    public int PictureFileId { get; set; }
-//    public string HomeAddress { get; set; }
-//}
+public class AppUser : IdentityUser<int>
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public bool IsActive { get; set; }
+    public int PictureFileId { get; set; }
+    public string HomeAddress { get; set; }
+}
--- App.InfraStructures.Database.SqlServer/AppDbContext.cs
+++ App.InfraStructures.Database.SqlServer/AppDbContext.cs
@@ -1,14 +1,11 @@
 ﻿using App.Domain.Core.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App.InfraStructures.Database.SqlServer
 {
-    public partial class AppDbContext : DbContext
+    public partial class AppDbContext : IdentityDbContext<AppUser, IdentityRole<int>, int>
     {
         public AppDbContext()
         {
